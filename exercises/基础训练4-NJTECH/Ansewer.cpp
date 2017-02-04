c9accd82e3a5b32c59d5f6925dc40601ff098b0d
znyTNT <zny_chers@hotmail.com>
2017-02-04 16:43:54 +0800

    4

---
+++ exercises/基础训练4-NJTECH/Ansewer.cpp
@@ -0,0 +1,119 @@
+10000
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"ave/5";
+}
+10001
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"day+=mon[i]";
+}
+10002
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"i%2==0";
+}
+10003
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"*haystack++!=*needle++";
+}
+10004
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"rank*rank*sizeof(int)\n";
+	cout<<"i%rank*rank+rank-1-i/rank";
+}
+10005
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"t=x[k];x[k]=x[i];x[i]=t;";
+}
+10006
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"swap(a,p,j)";
+}
+10007
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"*p_from";
+}
+10008
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"(width-2-strlen(buf))/2,\" \",buf,width-2-strlen(buf)-(width-strlen(buf)-2)/2,\" \"";
+}
+10009
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"while(m--)printf(\"---+\")";
+}
+10010
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"p++";
+}
+10011
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"NumRow!=3||NumCol!=3"<<endl;
+	cout<<"f(x,r,c+1)"<<endl;
+	cout<<"x[r][c]";
+}
+10014
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"a[b[k1]]>a[b[k2]]";
+}
+10015
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"x&(x+1)";
+}
+10016
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"if(r>0)return i";
+}
+10017
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"1"<<endl;
+	cout<<"pop(pst,buf,c,w)";
+}
+10018
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"n2/base+n3/base+n4%base"<<endl;
+	cout<<"r[2]/base";
+}
+10019
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"evaluate(x+1+v1.n)";
+}
+10020
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"j=next[j]";
+}
